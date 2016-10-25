f6e0f76aa7866a649de510ca01b1c3a0d1ea179f
Dominique Rolin <drolin@thoughtworks.com>
2016-10-26 09:03:58 +1000

    add C# test project

---
+++ Tests/AdminTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+
+namespace Tests
+{
+    [TestFixture]
+    public class AdminTests
+    {
+        [Test]
+        public void Admin_Can_Add_A_Movie()
+        {
+        }
+    }
+}
---
+++ Tests/UserTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+
+namespace Tests
+{
+    [TestFixture]
+    public class UserTests
+    {
+        [Test]
+        public void User_Can_List_All_Movies()
+        {
+        }
+        [Test]
+        public void User_Can_Rent_A_Movie()
+        {
+        }
+        [Test]
+        public void User_Can_List_Current_Rentals()
+        {
+        }
+    }
+}
